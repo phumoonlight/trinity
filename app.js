1749615289400ea231c8bacb68998d62fbb8d63c
panupong <panupong043@gmail.com>
2019-05-16 17:32:57 +0700

    update

--- app.js
+++ app.js
@@ -3,21 +3,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import User from './controllers/express';
 
+mongoose.connect('mongodb://localhost/mydb', { useNewUrlParser: true });
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/people', User.get);
-
 app.get('/people/:name', User.getid);
-
 app.post('/people', User.post);
-
 app.put('/people/:name', User.put);
-
 app.delete('/people/:id', User.deleteid);
-
 app.delete('/people', User.deleteall);
 
 app.listen(3000, () => {
--- controllers/mongoose.js
+++ controllers/mongoose.js
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost/mydb', { useNewUrlParser: true });
-
 export default mongoose.model('person', mongoose.Schema({
   firstname: String,
   lastname: String,
