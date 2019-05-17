552b104d813cced4a4c97e4db157cc511cd4cb59
Narubed Kaewserd <40911777+Narubed@users.noreply.github.com>
2019-05-17 09:34:17 +0700

    UpdatePassword 6 character

--- models/person.js
+++ models/person.js
@@ -3,6 +3,16 @@ import mongoose from 'mongoose';
 const personSchema = mongoose.Schema({
   firstname: String,
   lastname: String,
+  password: {
+    type: String,
+    validate: [
+      function (password) {
+        return password && password.length >= 6;
+      },
+      'Password 6 characters',
+    ],
+  },
+
 });
 
 const Person = mongoose.model('person', personSchema);
